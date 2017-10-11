f395772cd5241bb24367949efc25f53adfbfb381
Jens Jansson <peppe@vaadin.com>
2017-10-11 11:15:47 +0300

    The most basic grid example to try to get it working.

--- src/dashboard/OrderList.js
+++ src/dashboard/OrderList.js
@@ -4,50 +4,54 @@ import './OrderList.css';
 class OrderList extends Component {
 
   render() {
-    const orders = [
-      {
-        key: 1,
-        name: "one",
-        order: "hey",
-      },
-      {
-        key: 2,
-        name: "two",
-        order: "hey",
-      },
-      {
-        key: 3,
-        name: "three",
-        order: "hey",
-      },
-      {
-        key: 4,
-        name: "four",
-        order: "hey",
-      },
-      {
-        key: 5,
-        name: "five",
-        order: "hey",
-      }];
-    const listItems = orders.map((number) =>
-      <li key={number.key}>
-        {number.name}
-      </li>
-    );
+    // const orders = [
+    //   {
+    //     key: 1,
+    //     name: "one",
+    //     order: "hey",
+    //   },
+    //   {
+    //     key: 2,
+    //     name: "two",
+    //     order: "hey",
+    //   },
+    //   {
+    //     key: 3,
+    //     name: "three",
+    //     order: "hey",
+    //   },
+    //   {
+    //     key: 4,
+    //     name: "four",
+    //     order: "hey",
+    //   },
+    //   {
+    //     key: 5,
+    //     name: "five",
+    //     order: "hey",
+    //   }];
+    // const listItems = orders.map((number) =>
+    //   <li key={number.key}>
+    //     {number.name}
+    //   </li>
+    // );
     return (
       <div>
-        <vaadin-grid items="{orders}">
+        <vaadin-grid items='[{"name": "John", "surname": "Lennon", "role": "singer"},{"name": "Ringo", "surname": "Starr", "role": "drums"}]'>
           <vaadin-grid-column>
-            <template className="header">Hello</template>
-            <template>[[name]]</template>
+            <template className="header">Name</template>
+            <template>[[item.name]]</template>
           </vaadin-grid-column>
           <vaadin-grid-column>
-            <template className="header">World</template>
-            <template>[[order]]</template>
+            <template className="header">Surname</template>
+            <template>[[item.surname]]</template>
+          </vaadin-grid-column>
+          <vaadin-grid-column>
+            <template className="header">Role</template>
+            <template>[[item.role]]</template>
           </vaadin-grid-column>
         </vaadin-grid>
-        <ul>{listItems}</ul>
+        {/* <ul>{listItems}</ul> */}
         {/* <vaadin-grid items="[[users.result]]">
           <vaadin-grid-column width="50px" flex-grow="0">
             <template className="header">#</template>
