e1299fccc7c6b04119f37947dc2d3f259290e8eb
Jens Jansson <peppe@vaadin.com>
2018-01-22 15:10:58 +0200

    Indicator for active view in menu now changes based on navigation.

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 import './Header.css';
-import ViewItem from './ViewItem.js'
 import logo from '../logo.svg';
+import { NavLink } from 'react-router-dom'
 
 class Header extends Component {
   render() {
+    const baseUrl = process.env.PUBLIC_URL;
     return (
       <div className="header">
         <img src={logo} className="logo" alt="logo" />
         <div className="name">Café<br />Sunshine</div>
         <div className="views">
             <div className="container">
-              <ViewItem view="storefront" name="Storefront" />
-              <ViewItem view="dashboard" name="Dashboard" active="true" />
-              <ViewItem view="users" name="Users" />
-              <ViewItem view="products" name="Products" />
+              <NavLink to={baseUrl + "/storefront"} className="view" activeClassName="active">Storefront</NavLink>
+              <NavLink to={baseUrl + "/dashboard"} className="view" activeClassName="active">Dashboard</NavLink>
+              <NavLink to={baseUrl + "/users"} className="view" activeClassName="active">Users</NavLink>
+              <NavLink to={baseUrl + "/products"} className="view" activeClassName="active">Products</NavLink>
             </div>
         </div>
         <div className="avatar">
