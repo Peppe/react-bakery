1b0331b7c4cbb3b6f2437e30102ad0c26c99d4a9
Jens Jansson <peppe@vaadin.com>
2018-11-02 08:38:21 +0200

    updated to React hooks and tested it out in Users

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,22 +1,26 @@
-import React, { Component } from 'react';
+import  React, { useState, useEffect, useRef } from 'react';
 import '@vaadin/vaadin-text-field';
 import '@vaadin/vaadin-checkbox';
 import '@vaadin/vaadin-date-picker';
 import './Users.css';
 
-class Users extends Component {
-  render() {
+export default function Users() {
+    const [text, setText] = useState("hello");
+    const textField = useRef(null);
+    useEffect(() => {
+      textField.current.addEventListener("value-changed", (e) => {
+        setText(e.detail.value);
+      });
+    })
     return (
       <div className="users">
         <h3>Vaadin Core Elements</h3>
         <div className="controls">
-          <vaadin-text-field placeholder="This is vaadin-text-field"></vaadin-text-field>
+          <vaadin-text-field ref={textField} placeholder="This is vaadin-text-field" value={text}></vaadin-text-field>
           <vaadin-checkbox>and this, vaadin-checkbox</vaadin-checkbox>
           <vaadin-date-picker placeholder="fancy a vaadin-date-picker?"></vaadin-date-picker>
         </div>
+        {text}
       </div>
     );
-  }
 }
-
-export default Users;
